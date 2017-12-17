bd347d9f44fb5cf3775554aa06d67d6e5cb76d12
Unknown <nicolas.faqir@hotmail.fr>
2017-12-17 21:50:25 +0100

    Ajput pick up script
    Manque l'ajustement de la transform de l'arme après ramassage de l'arme

---
+++ Assets/PickUpScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.tag == "Player")
+        {
+            Debug.Log("Car has entered");
+            this.gameObject.transform.parent = c.transform;
+        }
+        
+    }
+}
